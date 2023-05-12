af26e75d3e268ba6b79a987b1701f0928e04f113
potso <jackdenmeade@gmail.com>
2023-05-12 18:58:49 +0100

    tidying the functions, instead of switch statements collapsing all options into a single object of nested arrays

--- message_generator/msgGen.js
+++ message_generator/msgGen.js
@@ -1,47 +1,32 @@
+const messageObj = {
+    luck: ["horrible luck!","bad luck.","good luck.","spectacular luck!"],
+    blessings: ["The Trees","The Mountains","The Stars"],
+    connectors: [
+        " tremble, today you will have ",
+        " bestow upon you ",
+        " bless you with ",
+        " present you with ",
+        " ponder, before giving you ",
+        " begrudgingly bless you with ",
+        " bequeath unto you the finest ",
+        " dole out ",
+        " impart upon you ",
+        " offer up their "
+    ]
+};
+
 function getLuckLvl() {
     let luck = Math.floor(Math.random()*3);
     return luck;
 };
 
 function getBlesser() {
-    let blessNum = Math.floor(Math.random()*2);
-    let blesser = "";
-    switch (blessNum){
-        case 0:
-            blesser = "The Trees"
-        case 1:
-            blesser = "The Mountains"
-        case 2:
-            blesser = "The Stars"
-    };
+    let blesser = Math.floor(Math.random()*2);
     return blesser;
 };
 
 function getConnector() {
-    let connectorNum = Math.floor(Math.random()*9);
-    let connector = "";
-    switch (blessNum){
-        case 0:
-            connector = " tremble, today you will have "
-        case 1:
-            connector = " bestow upon you "
-        case 2:
-            connector = " bless you with "
-        case 3:
-            connector = " present you with "
-        case 4:
-            connector = " ponder, before giving you "
-        case 5: 
-            connector = " begrudgingly bless you with "
-        case 6: 
-            connector = " bequeath unto you the finest "
-        case 7:
-            connector = " dole out "
-        case 8:
-            connector = " impart upon you "
-        case 9:
-            connector = " offer up their "
-    };
+    let connector = Math.floor(Math.random()*9);
     return connector;
 };
 
